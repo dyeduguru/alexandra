c6b20832b7be0741f40bb675b696cd2f1f635024
Kaushik Iska <kaushik@safegraph.com>
2018-06-17 22:19:43 -0700

    memtable flush

--- memtable-impl/src/main/java/com/pichafast/db/memtable/impl/SkipListMemTable.java
+++ memtable-impl/src/main/java/com/pichafast/db/memtable/impl/SkipListMemTable.java
@@ -1,22 +1,28 @@
 package com.pichafast.db.memtable.impl;
 
+import apple.laf.JRSUIUtils.Tree;
 import com.google.common.base.Preconditions;
 import com.pichafast.db.KeyNotFoundException;
+import com.pichafast.db.foundation.Row;
+import com.pichafast.db.sstable.SSTable;
+import com.pichafast.db.sstable.SSTableManager;
+import com.pichafast.db.sstable.SSTableManagerImpl;
 import com.pichafast.db.storage.StorageTable;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Collectors;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 
 public class SkipListMemTable implements StorageTable {
 
   private final ConcurrentSkipListMap<String, String> map;
+  private final SSTableManager ssTableManager;
 
   public SkipListMemTable() {
     this.map = new ConcurrentSkipListMap<>();
-  }
-
-  private SkipListMemTable(ConcurrentSkipListMap<String, String> map) {
-    this.map = map;
+    this.ssTableManager = new SSTableManagerImpl();
   }
 
   public void put(byte[] key, byte[] val) {
@@ -41,6 +47,15 @@ public class SkipListMemTable implements StorageTable {
   public boolean hasKey(byte[] key) {
     Preconditions.checkNotNull(key, "null key encountered.");
     return map.containsKey(toHex(key));
+  }
+
+  @Override
+  public SSTable flush() {
+    SortedSet<Row> rows = map.entrySet()
+        .stream()
+        .map(it -> new Row(it.getKey(), it.getValue()))
+        .collect(Collectors.toCollection(TreeSet::new));
+    return ssTableManager.create(rows);
   }
 
   private static String toHex(byte [] bytes) {
