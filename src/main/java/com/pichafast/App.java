751f5466aeb6430a13d5ced3014d0236e4a938fa
Dinesh Yeduguru <dyeduguru@gmail.com>
2018-06-17 19:52:14 -0700

    init

---
+++ src/main/java/com/pichafast/App.java
@@ -0,0 +1,13 @@
+package com.pichafast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
